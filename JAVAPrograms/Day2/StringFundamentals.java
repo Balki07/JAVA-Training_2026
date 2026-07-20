b8f637f66294cfc7ba3b5edce65ce7ffe0252bff
balaganeshr <rbalaganesh2307@gmail.com>
2026-07-20 11:07:36 +0530

    Add files via upload

---
+++ JAVAPrograms/Day2/StringFundamentals.java
@@ -0,0 +1,13 @@
+class StringFundamentals{
+    public static void main(String[] args) {
+        
+    String S1="Bala";
+    String S2="Ganesh";
+    System.out.println("Length of S1 is "+S1.length());
+    System.out.println("Character at index 2 of S1 is "+S1.charAt(2));
+    System.out.println("Are S1 and S2 equal? "+S1.equals(S2));
+    System.out.println("Concatenation of S1 and S2 is "+S1.concat(S2));
+    System.out.println("The Uppercase of S1 is "+S1.toUpperCase());
+    System.out.println("The Lowercase of S1 is "+S1.toLowerCase());
+    }
+}
